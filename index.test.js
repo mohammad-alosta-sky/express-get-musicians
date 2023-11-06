20765c04fb7d4ac8aeb32974a20551b1f6996b0d
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-11-06 14:41:50 +0000

    Extension Problems done

--- index.test.js
+++ index.test.js
@@ -1,18 +1,49 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+// const { execSync } = require('child_process');
+// execSync('npm install');
+// execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
+const {describe, test, expect} = require("@jest/globals")
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
     
+    test("get 200 status",async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+
+    })
+
+    test("test the data", async () => {
+        const response = await request(app).get("/musicians");
+
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0].name).toBe("Mick Jagger")
+    })
+    
+})
+
+describe('./musicians endpoint', () => {
+    // Write your tests here
+    
+    test("get 200 status",async () => {
+        const response = await request(app).get("/bands");
+        expect(response.statusCode).toBe(200);
+
+    })
+
+    test("test the data", async () => {
+        const response = await request(app).get("/bands");
+
+        const responseData = JSON.parse(response.text);
+        expect(responseData[0].name).toBe("The Beatles")
+    })
     
 
 
--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+    const allMusicians = await Musician.findAll();
+    res.json(allMusicians);
+})
 
+
+app.get("/musicians/:idTag", async (req, res) => {
+    const allMusicians = await Musician.findByPk(req.params.idTag);
+    res.json(allMusicians);
+})
+
+
+app.get("/bands", async (req, res) => {
+    const allBands = await Band.findAll();
+    res.json(allBands);
+
+})
 
 
 
